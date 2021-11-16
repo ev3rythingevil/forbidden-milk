734df13bedb22885e02119793fe8a8abafdd64a9
Hathor Hamilton <koshknot@gmail.com>
2021-11-16 18:06:56 -0500

    navbar basic funcitons

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import SplashPage from './SplashPage';
+import { useState } from 'react';
+import NavBar from './NavBar';
 
 function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+
+const [user, setUser] = useState([])
+
+
+function userLogIn(saveData){
+    console.log(saveData)
 }
+  
+  
+  if (/*user logged in*/ user)
+  return(
+    <div>
+      <NavBar userLogIn={userLogIn}/>
+    <h1>Welcome User, here's some stuff to look at</h1>
+    </div>
+  )
+
+  else /* no user logged in */
+    return (
+      <div>
+        <NavBar />
+        <SplashPage />
+      </div>
+    );
+  }
 
 export default App;
--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,6 +1,39 @@
-function NavBar(){
+import { useState } from 'react'
+
+function NavBar({userLogIn}){
+    
+    const [saveData, setSaveData] = useState({})
+    
+    function handleChange(e){
+        setSaveData({...saveData, 
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    function logInData(e){
+        e.preventDefault()
+        userLogIn(saveData)
+    }
+ 
+    
+
+    
     return(
-        <h1>Hello, I love you wont you tell me your name?</h1>
+        <div>
+        <h1>WaxStax</h1>
+        <form onSubmit={e => logInData(e)}>
+            <label>
+                Username:
+                <input type="text" name="username" onChange={(e)=> handleChange(e)}/>
+            </label>
+            <label>
+                Password:
+                <input type="text" name="password" onChange={(e)=> handleChange(e)}/>
+            </label>
+            <input type="submit" value="Log In"/>
+        </form>
+        </div>
+
     )
 }
 
